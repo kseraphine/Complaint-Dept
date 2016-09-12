4e4ab7c8ec3b8ff9f20cedceaff6e8a6b0fb2237
Kelana Latimer <Kelana Latimer>
2016-09-12 18:47:12 -0500

    updating api files

--- assets/javascript/complaints.js
+++ assets/javascript/complaints.js
@@ -1,12 +1,21 @@
 // How do we use the APIs?
 //
-var keywords;
-var key;
-var farooqueryURL = 'http://www.faroo.com/api?q=iphone&start=1&length=10&l=en&src=web&i=false&f=json' + key;
+var keywords = 'wife-funny-meme';
+var key = 'AIzaSyB-LwbjvB0YnRRuGl-dV3VGGx66ujm-fck';
+var cx = '003192956300846753352:1j_2oos-ga0'
+var googlequeryURL = 'https://www.googleapis.com/customsearch/v1?key=' + key + '&cx=' + cx + '&searchType=image&q=' + keywords;
 var youtubequeryURL = 'https://www.googleapis.com/youtube/v3/search';
 
 $(document).ready(function(){
 
+  $.ajax({ url: googlequeryURL, method: 'GET' })
+    .done(function(results) {
+      console.log("------------------------------------")
+      console.log("URL: " + googlequeryURL);
+      console.log("------------------------------------")
+      console.log(results);
+      console.log("------------------------------------")
+    })
 
 
 
