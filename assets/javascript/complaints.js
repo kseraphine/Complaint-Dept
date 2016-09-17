43731ea70708a4b2e5382c44036f4d10a2cf833d
Maria Beane <mbeane@findyr.com>
2016-09-17 10:09:30 -0500

    set up database initialization. Catrgory, complaint, and keywords set and snapshot setup.

--- assets/javascript/complaints.js
+++ assets/javascript/complaints.js
@@ -7,12 +7,17 @@
   };
   firebase.initializeApp(config);
 
+  var database = firebase.database();
+
 //global variables
+var category;
+var complaint;
+var keywords;
 
 
 // How do we use the APIs?
 //
-var keywords = 'wife-funny-meme';
+//var keywords = 'wife-funny-meme';
 var key = 'AIzaSyB-LwbjvB0YnRRuGl-dV3VGGx66ujm-fck';
 var cx = '003192956300846753352:1j_2oos-ga0'
 var googlequeryURL = 'https://www.googleapis.com/customsearch/v1?key=' + key + '&cx=' + cx + '&searchType=image&q=' + keywords;
@@ -26,6 +31,54 @@ $(document).ready(function(){
     //Get keywords from firebase
 
   //Else if they add a complaint add it, then call search.
+
+
+//var path = adaFirstNameRef.toString();
+
+  $('#submit-complaint').on('click', function() {
+    /*
+      var complaintCategoryRef = firebase.database().ref('family');
+      var complaintRef = complaintCategoryRef.child('complaint');
+      var complaintKeywordsRef = complaintRef.child('keywords');
+    */
+      var complaint = $('#complaint').val();
+      var complaintKeywords = $('#keywords').val().trim().replace(/,/g, '').split(" ");
+      var complaintCategory = $('#category').text();
+
+      var newComplaint = {
+        complaint: complaint,
+        keywords: complaintKeywords,
+      }
+
+      if (complaintCategory == 'Family') {
+        database.ref('family').push(newComplaint);
+      } else if (complaintCategory == 'Job') {
+        database.ref('job').push(newComplaint);
+      } else if (complaintCategory == 'School') {
+        database.ref('school').push(newComplaint);
+      };
+      console.log(newComplaint.complaint);
+      console.log(newComplaint.keywords);
+      console.log(complaintCategory);
+
+      return false;
+
+  });
+
+  //display the complaints
+    database.ref('family').on("child_added", function(childSnapshot) {
+
+      console.log(childSnapshot.val());
+
+      var currentComplaint = childSnapshot.val().complaint;
+      var currentKeywords = childSnapshot.val().keywords;
+
+      console.log(currentComplaint);
+      console.log(currentKeywords);
+
+    });
+
+
     //Bring up a modal
 
     //User enters description and keywords
